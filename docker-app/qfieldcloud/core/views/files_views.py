ca07cd76d267cc38ff5a6aea3fa3484080365401
Ivan Ivanov <ivan.ivanov@suricactus.com>
2023-08-01 18:41:38 +0300

    asd

--- docker-app/qfieldcloud/core/views/files_views.py
+++ docker-app/qfieldcloud/core/views/files_views.py
@@ -361,28 +361,3 @@ class FilesListView(generics.ListAPIView):
 
     def get_queryset(self):
         return File.objects.all()
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def perform_destroy(self, instance):
-        instance.delete()
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
-    def perform_create(self, serializer):
-        serializer.save()
